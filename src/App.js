108836777f81db2404b2f3caf24e063fc7ae71ff
Nancy <duong.nancy@gmail.com>
2024-11-03 19:39:52 -0800

    Added search engine to project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      Hello
-    </div>
+      <div className="container">
+        <header className="App-header">
+        Hello
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Nancy Duong</small>
+        </footer>
+      </div>
+      </div>
   );
 }
 
